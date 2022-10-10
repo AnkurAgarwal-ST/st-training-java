85c3b57bc70e658f59a588a70a71cb6f8d58fb07
Ankur Agarwal <89902473+AnkurAgarwal-ST@users.noreply.github.com>
2022-10-10 13:43:14 +0530

    formatting change and java doc

--- src/main/java/com/symphonytalent/candidate/service/CandidateService.java
+++ src/main/java/com/symphonytalent/candidate/service/CandidateService.java
@@ -3,7 +3,11 @@ package com.symphonytalent.candidate.service;
 import java.util.List;
 import com.symphonytalent.candidate.bean.Candidate;
 
-public interface CandidateService {
+/*
+* the candidate service
+*/
+public interface CandidateService 
+{
 
   List<Candidate> getAll();
 }
