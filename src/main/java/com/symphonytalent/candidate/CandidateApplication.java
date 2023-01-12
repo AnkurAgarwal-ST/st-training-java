d6b09f6a38bf7c8b606cef4d33068283b7ee9b0e
Ankur Agarwal <89902473+AnkurAgarwal-ST@users.noreply.github.com>
2023-01-12 16:17:32 +0530

    dummy

--- src/main/java/com/symphonytalent/candidate/CandidateApplication.java
+++ src/main/java/com/symphonytalent/candidate/CandidateApplication.java
@@ -10,5 +10,6 @@ public static void main(String[] args)
 SpringApplication.run(CandidateApplication.class, args);
 System.out.println("Open Application ->  http://localhost:8080");
 System.out.println("Open Application at ->  http://localhost:8082");
+
 }
 }
